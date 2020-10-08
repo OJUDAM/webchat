fdae04514ca62762c75b9a70fcb69a89a9a1df2c
오주담 <dhwnkea@naver.com>
2020-10-08 15:33:05 +0900

    UserController url mapping

---
+++ src/main/java/com/example/webchat/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.example.webchat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@RequestMapping("/join")
+	public String join() {
+		return "/WEB-INF/user/join.jsp";
+	}
+}
