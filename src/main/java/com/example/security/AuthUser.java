f6d8ccd513205a0d6eb29f1c1f2ac76e0d50d615
오주담 <dhwnkea@naver.com>
2020-10-14 21:02:13 +0900

    @AuthUser modify

--- src/main/java/com/example/security/AuthUser.java
+++ src/main/java/com/example/security/AuthUser.java
@@ -1,13 +1,12 @@
 package com.example.security;
 
-import java.lang.annotation.ElementType;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention( RetentionPolicy.RUNTIME)
-@Target( {ElementType.PARAMETER})
+@Retention(RUNTIME)
+@Target(PARAMETER)
 public @interface AuthUser {
-	public enum Role {ADMIN, USER}
-	Role value() default Role.USER;
 }
