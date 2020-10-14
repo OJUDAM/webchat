23f50e6f6a773ace0b83246d5e15043dd752c74d
오주담 <dhwnkea@naver.com>
2020-10-14 19:06:25 +0900

    modify 수정 5

--- src/main/java/com/example/webchat/controller/UserController.java
+++ src/main/java/com/example/webchat/controller/UserController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -59,9 +60,9 @@ public class UserController {
 	
 	@Auth(Auth.Role.USER)
 	@RequestMapping(value="/modify", method=RequestMethod.GET)
-	public String modify(@ModelAttribute @AuthUser UserVo userVo) {
-		userVo = userService.getUser( userVo.getNo() );
-		
+	public String modify(@AuthUser UserVo authUser, Model model) {
+		UserVo userVo = userService.getUser( authUser.getNo() );
+		model.addAttribute("userVo", userVo);
 		return "/WEB-INF/views/user/modify.jsp";
 	}
 }
