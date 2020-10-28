0526815d362a50e8334346482236875122e1f780
오주담 <dhwnkea@naver.com>
2020-10-28 15:01:31 +0900

    test

--- src/main/java/com/example/webchat/controller/MainController.java
+++ src/main/java/com/example/webchat/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.example.webchat.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,7 +10,9 @@ import com.example.webchat.service.WebchatService;
 @Controller
 public class MainController {
 	
+	@Autowired
 	private WebchatService webchatService;
+	
 	@RequestMapping({"/","/main"})
 	public String index() {
 		return "/WEB-INF/views/main/index.jsp";
