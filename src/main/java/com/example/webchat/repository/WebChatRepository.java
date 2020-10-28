7802ca6a3776d37475563e6420a74ac3d0d2ac89
오주담 <dhwnkea@naver.com>
2020-10-28 12:15:45 +0900

    @Repository test

--- src/main/java/com/example/webchat/repository/WebChatRepository.java
+++ src/main/java/com/example/webchat/repository/WebChatRepository.java
@@ -1,15 +1,16 @@
 package com.example.webchat.repository;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.stereotype.Repository;
+
 import com.opencsv.CSVReader;
 
+@Repository
 public class WebChatRepository {
 
 	public Map<String, String> getNickName() {
