5995935bfdf1f027081c0674eea856d14558b838
오주담 <dhwnkea@naver.com>
2020-10-28 14:42:07 +0900

    test

--- src/main/java/com/example/webchat/controller/WebSocketChat.java
+++ src/main/java/com/example/webchat/controller/WebSocketChat.java
@@ -30,7 +30,7 @@ public class WebSocketChat {
 	
 	@OnOpen
 	public void onOpen(Session session) {
-		//System.out.println(webchatService.getNickName());
+		System.out.println(webchatService.getNickName());
 		System.out.println("Open session id: "+session.getId());
 		
 		try {
